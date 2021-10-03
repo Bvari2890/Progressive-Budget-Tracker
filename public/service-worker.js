a90e5677fdf02bfad08d4e99dfd5aa7931f3e72c
Bo Vari <bvari2890@gmail.com>
2021-10-02 21:00:26 -0400

    adding all content

---
+++ public/service-worker.js
@@ -0,0 +1,66 @@
+const CACHE_NAME = "budget-cache";
+const DATA_CACHE_NAME = "budget-data-cache";
+
+const FILES_TO_CACHE = [
+    "/", 
+    "/index.html", 
+    "/index.js", 
+    "/favicon.ico", 
+    "/styles.css",
+    "/icons/icon-144X144.png",
+    "/icons/icon-192x192.png", 
+    "/icons/icon-512x512.png"
+  ];
+  
+  self.addEventListener("install", function (evt) {
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    );
+    self.skipWaiting();
+  });
+  
+  self.addEventListener("activate", function (evt) {
+    evt.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
+    self.clients.claim();
+  });
+
+  self.addEventListener("fetch", function (evt) {
+    if (evt.request.url.includes("/api")) {
+      evt.respondWith(
+        caches.open(DATA_CACHE_NAME).then((cache) => {
+          return fetch(evt.request)
+            .then((response) => {
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+  
+              return response;
+            })
+            .catch((err) => {
+              return cache.match(evt.request);
+            });
+        })
+      );
+  
+      return;
+    }
+
+    evt.respondWith(
+        caches.open(CACHE_NAME).then((cache) => {
+          return cache.match(evt.request).then((response) => {
+            return response || fetch(evt.request);
+          });
+        })
+      );
+    });
